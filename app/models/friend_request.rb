f70ea6c0ff85c9650cad022ae811d71bff68ceb7
cgcarlosg <cgcarlosg@hotmail.com>
2021-01-24 21:33:20 -0500

    fix rubocop issues

--- app/models/friend_request.rb
+++ app/models/friend_request.rb
@@ -5,7 +5,8 @@ class FriendRequest < ApplicationRecord
   scope :pending, -> { where(accepted: false) }
   scope :accepted, -> { where(accepted: true) }
   scope :other_user_id, lambda { |user_id|
-                          select("CASE WHEN requestee_id = #{user_id} THEN requester_id WHEN requester_id = #{user_id} 
+                          select("CASE WHEN requestee_id = #{user_id} THEN requester_id WHEN requester_id = #{user_id}
                           THEN requestee_id END AS user_id")
-                            .where("requestee_id = #{user_id} OR requester_id = #{user_id}") }
+                            .where("requestee_id = #{user_id} OR requester_id = #{user_id}")
+                        }
 end
