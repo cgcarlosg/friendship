511ee19763c42b1529784cba4e27e7ee67ba79cd
cgcarlosg <cgcarlosg@hotmail.com>
2021-01-26 15:29:25 -0500

    fix spec

--- spec/friendrequest_spec.rb
+++ spec/friendrequest_spec.rb
@@ -1,19 +1,5 @@
 require 'rails_helper'
 
-RSpec.describe 'Users', type: :request do
-  context '#Controller' do
-    it 'Enable to create a new user' do
-      get '/users/sign_up'
-      assert_response :success
-    end
-  end
-
-  it 'name must not be in blank' do
-    user = User.new(name: '').save
-    expect(user).to eql(false)
-  end
-end
-
 RSpec.describe User, type: :model do
   describe 'validation' do
     user = User.new(email: 'one@gmail.com', name: 'one', password: '111111')
@@ -25,5 +11,13 @@ RSpec.describe User, type: :model do
     it 'should have a valid user' do
       expect(User.new(name: 'two', email: 'two@gmail.com', password: '111111')).to be_valid
     end
+  end
+end
+
+RSpec.describe 'Friendship', type: :model do
+  it 'will check for create a valid friendship ' do
+    f = User.create!(email: 'test1@gmail.com', name: 'moon', password: '123456')
+    v = Friendship.new(user_id: f.id)
+    expect(v).to_not be_valid
   end
 end
