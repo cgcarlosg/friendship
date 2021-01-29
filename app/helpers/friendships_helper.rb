f3b02b4f0873b74b32a9724f1e103dcaac8853d1
Carlos Albenio Gutierrez Monroy <cgcarlosg@hotmail.com>
2021-01-28 23:48:48 -0500

    Update friendships_helper.rb

--- app/helpers/friendships_helper.rb
+++ app/helpers/friendships_helper.rb
@@ -2,6 +2,7 @@ module FriendshipsHelper
   def list_friends
     arr = ''
     return if @users.nil?
+
       @friends.each do |f|
         puts f
         arr << "<li>#{f.user.name} #{f.confirmed ? '' : ', pending'}<li>"
