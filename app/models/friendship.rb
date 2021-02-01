fe16d34cf145f8abaa311de3a338e4d84611e6e4
Carlos Albenio Gutierrez Monroy <cgcarlosg@hotmail.com>
2021-02-01 17:43:15 -0500

    Create friendship.rb

---
+++ app/models/friendship.rb
@@ -0,0 +1,12 @@
+class Friendship < ApplicationRecord
+  after_save :create_friendship
+  belongs_to :user
+  belongs_to :friend, class_name: 'User'
+
+  def create_friendship
+    return if Friendship.where(user_id: friend_id, friend_id: user_id) != []
+
+    user_friendship1 = Friendship.create(user_id: friend_id, friend_id: user_id)
+    user_friendship1.save
+  end
+end
